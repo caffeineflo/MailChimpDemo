28bd834e159d6044724f8ddcff76fa7b38134872
Florian Harr <harr.florian@gmx.de>
2017-03-15 16:29:16 -0400

    Initial Commit

---
+++ MailChimp Chimperator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MailChimp Chimperator
+//
+//  Created by Florian Harr on 3/15/17.
+//  Copyright © 2017 Florian Harr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
